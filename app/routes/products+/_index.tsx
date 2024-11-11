9a564147a4c5ef77bfdc7a7e3d6af6728219d984
Artem Sedliar <artemsedlyar99@gmail.com>
2024-11-11 13:32:25 +0200

    refactor: comment out unused imports and redirect

--- app/routes/products+/_index.tsx
+++ app/routes/products+/_index.tsx
@@ -1,15 +1,15 @@
 import type {MetaFunction} from '@remix-run/node';
-import {redirect} from '@remix-run/react';
-import {useTranslation} from 'react-i18next';
+// import {redirect} from '@remix-run/react';
+// import {useTranslation} from 'react-i18next';
 
 import {Stack} from '@mui/material';
 
-import {useQueryProductsList} from '~/services/products';
+// import {useQueryProductsList} from '~/services/products';
 
-import {SkeletonOnLoading} from '~/global/components/skeleton-on-loading';
-import {AppButton} from '~/global/components/app-button';
+// import {SkeletonOnLoading} from '~/global/components/skeleton-on-loading';
+// import {AppButton} from '~/global/components/app-button';
 
-import {ProductsTable} from './components/table';
+// import {ProductsTable} from './components/table';
 
 //
 //
@@ -17,18 +17,18 @@ import {ProductsTable} from './components/table';
 export const handle = {i18n: ['common', 'products']};
 export const meta: MetaFunction = () => [{title: 'Remix App - Products'}];
 
-export const clientLoader = async () => {
-  if (!window.localStorage.getItem('_at')) return redirect('/');
+// export const clientLoader = async () => {
+//   if (!window.localStorage.getItem('_at')) return redirect('/');
 
-  return null;
-};
+//   return null;
+// };
 
 //
 //
 
 export default function Products() {
-  const {t} = useTranslation(['common']);
-  const {data, isLoading} = useQueryProductsList();
+  // const {t} = useTranslation(['common']);
+  // const {data, isLoading} = useQueryProductsList();
 
   //
   //
@@ -36,14 +36,10 @@ export default function Products() {
   return (
     <>
       <Stack alignItems="flex-end" my={2}>
-        <SkeletonOnLoading isLoading={isLoading}>
-          <AppButton to="/products/create" variant="contained">
-            {t('common:create')}
-          </AppButton>
-        </SkeletonOnLoading>
+        <div>123</div>
       </Stack>
 
-      <ProductsTable data={data?.result} isLoading={isLoading} />
+      {/* <ProductsTable data={data?.result} isLoading={isLoading} /> */}
     </>
   );
 }
